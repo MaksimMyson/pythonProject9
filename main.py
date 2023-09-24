5b1262e9a64d4ba16645814cb3dc2ceafe0cac37
IT STEP <memasik4you@gmal.com>
2023-09-24 11:27:22 +0300

    ex 2

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-a = int(input())
-b = int(input())
+begin = int(input("Початок:"))
+end = int(input("Кінець:"))
+while begin <= end:
+begin += 1
+print(begin, end="\t")
+if begin % 7 == 0:
+print(f"| % 7", end="")
+if begin % 5 == 0:
+print(f"| % 5", end="")
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
