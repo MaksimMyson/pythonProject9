ca4ccb6c49256264dfd98fe991457b2d75a420a1
Mayson Kotlyar Maksim <143324782+MaksimMyson@users.noreply.github.com>
2023-09-24 14:05:49 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = int(input(" Число для вичисления факториала: "))
-factorial = 1
-count = 1
-while count <= n:
-    factorial   *= count
+length = int(input(" длина линии"))
+count = 0
+while count < length:
+    print("*", end='')
     count += 1
-    print(f"Факториал числа {n} равняется {factorial}")
+print()
+
