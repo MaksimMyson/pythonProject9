6401f4da8a4a28764fbc57833eac420dc4148282
IT STEP <memasik4you@gmal.com>
2023-09-24 11:38:14 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 begin = int(input("Початок:"))
 end = int(input("Кінець:"))
 while begin <= end:
-begin += 1
-print(begin, end="\t")
-if begin % 7 == 0:
-print(f"| % 7", end="")
-if begin % 5 == 0:
-print(f"| % 5", end="")
-print()
+    begin += 1
+    print(begin, end="\t")
+    if begin % 3 == 0:
+        print(f"| Fizz", end="")
+        if begin % 5 == 0:
+            print(f"| Buzz", end="")
+            if begin % 3 == 0 and  begin % 5 == 0:
+                print(f"| Fizz Buzz")
+            print()
+
+
+
+
+
+
 
 
 
