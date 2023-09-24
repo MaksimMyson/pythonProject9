98b2db26a9f2bf7b58911b2183083b7732866f7a
Mayson Kotlyar Maksim <143324782+MaksimMyson@users.noreply.github.com>
2023-09-24 13:38:02 +0300

    ex 1

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-a = int(input())
-b = int(input())
+start = int(input(" початок діапазону: "))
+end = int(input(" кінець діапазону: "))
+sum_of_numbers = 0
+count = 0
+current_number = start
+while current_number <= end:
+    sum_of_numbers += current_number
+    count += 1
+    current_number += 1
+average = sum_of_numbers / count
+print(f"Сума чисел у діапазоні: {sum_of_numbers}")
+print(f"Середня арифметичнe у діапазоні: {average}")
